03c72065583a177cee795db9e03f5259a1be7090
DemetriousWilson <DemetriousWilson@gmail.com>
2013-11-07 21:50:10 -0800

    Update HelloSpark add some random routes

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -9,5 +9,20 @@ public class HelloSpark {
                 return "Hello Spark MVC Framework!";
             }
         });
+
+        get(new Route("/goodbye") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Goodbye Spark MVC Framework!";
+            }
+        });
+
+        get(new Route("/parameter/:param") {
+            @Override
+            public Object handle(Request request, Response response) {
+                StringBuffer myParam = new StringBuffer(request.params(":param"));
+                return "I reversed your param for ya \"" + myParam.reverse() + "\"";
+            }
+        });
     }
 }
