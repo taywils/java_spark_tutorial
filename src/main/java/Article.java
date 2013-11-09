4a64adee0204a6d9e98d0c954c019941d5e22158
DemetriousWilson <DemetriousWilson@gmail.com>
2013-11-08 22:13:29 -0800

    Blog part 1, Create

---
+++ src/main/java/Article.java
@@ -0,0 +1,54 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Article {
+    private String title;
+    private String content;
+    private Date createdAt;
+    private String summary;
+    private Integer id;
+    private Boolean deleted;
+
+    public Article(String title, String summary, String content, Integer size) {
+        this.title = title;
+        this.summary = summary;
+        this.content = content;
+        this.createdAt = new Date();
+        this.id = size;
+        this.deleted = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSummary() {
+        return "<a href='/article/read/" + this.id + "'>" + this.summary + "</a>";
+    }
+
+    public void delete() {
+        this.deleted = true;
+    }
+
+    public Boolean readable() {
+        return this.deleted;
+    }
+
+    public String getCreatedAt() {
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        return dateFormat.format(this.createdAt);
+    }
+
+    public String getEditLink() {
+        return "<a href='/article/update/" + this.id + "'>Edit</a>";
+    }
+
+    public String getDeleteLink() {
+        return "<a href='/article/delete/" + this.id + "'>Delete</a>";
+    }
+}
